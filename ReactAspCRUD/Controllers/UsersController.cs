25f3e6b7d8b06247d29994db461ae472366d877a
Dmitriy Pestov <dmitriy.pestov@valtech.com>
2020-09-10 15:03:51 +0300

    First commit

---
+++ ReactAspCRUD/Controllers/UsersController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using ReactAspCRUD.Models;
+using ReactAspCRUD.Services;
+
+namespace ReactAspCRUD.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("ReactPolicy")]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserService _userService;
+        public UsersController(UserService userService)
+        {
+            this._userService = userService;
+        }
+
+        // GET api/users
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return _userService.GetAll();
+        }
+
+        // GET api/users/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            return Ok(_userService.GetById(id));
+        }
+
+        // POST api/users
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] User user)
+        {
+            return CreatedAtAction("Get", new { id = user.Id }, _userService.Create(user));
+        }
+
+        // PUT api/users/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] User user)
+        {
+            _userService.Update(id, user);
+            return NoContent();
+        }
+
+        // DELETE api/users/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            _userService.Delete(id);
+            return NoContent();
+        }
+        public override NoContentResult NoContent()
+        {
+            return base.NoContent();
+        }
+    }
+}
---
+++ ReactAspCRUD/Services/UserService.cs
@@ -0,0 +1,63 @@
+﻿using ReactAspCRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ReactAspCRUD.Services
+{
+    public class UserService
+    {
+        private static List<User> users = new List<User>();
+        private static int Count = 1;
+        private static readonly string[] names = new string[] { "Jonathan", "Mary", "Susan", "Joe", "Paul", "Carl", "Amanda", "Neil" };
+        private static readonly string[] surnames = new string[] { "Smith", "O'Neil", "MacDonald", "Gay", "Bailee", "Saigan", "Strip", "Spenser" };
+        private static readonly string[] extensions = new string[] { "@gmail.com", "@hotmail.com", "@outlook.com", "@icloud.com", "@yahoo.com" };
+        static UserService()
+        {
+            Random rnd = new Random();
+            for(int i = 0; i<5; i++)
+            {
+                string currName = names[rnd.Next(names.Length)];
+                User user = new User
+                {
+                    Id = Count++,
+                    Name = currName + " " + surnames[rnd.Next(surnames.Length)],
+                    Email = currName.ToLower() + extensions[rnd.Next(extensions.Length)],
+                    Document = (rnd.Next(0, 100000) * rnd.Next(0, 100000)).ToString().PadLeft(10, '0'),
+                    Phone = "+3 8066-12-44-452"
+                };
+                users.Add(user);
+            }
+        }
+
+        public List<User> GetAll()
+        {
+            return users;
+        }
+
+        public User GetById(int id)
+        {
+            return users.Where(user => user.Id == id).FirstOrDefault();
+        }
+
+        public User Create(User user)
+        {
+            user.Id = Count++;
+            users.Add(user);
+            return user;
+        }
+        public void Update(int id, User user)
+        {
+            User found = users.Where(u => u.Id == id).FirstOrDefault();
+            found.Name = user.Name;
+            found.Email = user.Email;
+            found.Phone = user.Phone;
+            found.Document = user.Document;
+        }
+
+        public void Delete(int id)
+        {
+            users.RemoveAll(u => u.Id == id);
+        }
+    }
+}
